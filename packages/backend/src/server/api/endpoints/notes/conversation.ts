649efcbca18887ec77573df9d36dc5cbcacf5231
丈槍由紀 <chikorita157@ateliershiori.moe>
2024-08-28 17:15:22 -0400

    Revert "Update conversation.ts"
    This reverts commit 0399132d577892b2e4edb82fdad25a8d05dbb85f.

--- packages/backend/src/server/api/endpoints/notes/conversation.ts
+++ packages/backend/src/server/api/endpoints/notes/conversation.ts
@@ -11,8 +11,6 @@ import { NoteEntityService } from '@/core/entities/NoteEntityService.js';
 import { DI } from '@/di-symbols.js';
 import { GetterService } from '@/server/api/GetterService.js';
 import { ApiError } from '../../error.js';
-import { MetaService } from '@/core/MetaService.js';
-import { UtilityService } from '@/core/UtilityService.js';
 
 export const meta = {
 	tags: ['notes'],
@@ -58,7 +56,6 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 		private getterService: GetterService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
-			const metaSvc = await this.metaService.fetch(true);
 			const note = await this.getterService.getNote(ps.noteId).catch(err => {
 				if (err.id === '9725d0ce-ba28-4dde-95a7-2cbb2c15de24') throw new ApiError(meta.errors.noSuchNote);
 				throw err;
@@ -71,14 +68,9 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 				i++;
 				const p = await this.notesRepository.findOneBy({ id });
 				if (p == null) return;
-				let badReply = false;
-					if (p.user?.isSilenced && me && followings && p.userId == me.id && followings[p.userId]) {badReply = true;}
-					if (!me && p.user?.isSilenced) {badReply = true;}
-					if (p.user?.isSuspended) {badReply = true;}
-					if (this.utilityService.isBlockedHost(metaSvc.blockedHosts, p.userHost)) {badReply = true;}
-					if (this.utilityService.isSilencedHost(metaSvc.silencedHosts, p.userHost)) {badReply = true;}
-				if (i > ps.offset! && !badReply) {
-						conversation.push(p);
+
+				if (i > ps.offset!) {
+					conversation.push(p);
 				}
 
 				if (conversation.length === ps.limit) {
