e2e96b657efe5ec66b5b5be899dcb0a9c45396a1
丈槍由紀 <chikorita157@ateliershiori.moe>
2024-08-28 17:30:29 -0400

    Debug children as to why notes are not loading in child

--- packages/backend/src/server/api/endpoints/notes/children.ts
+++ packages/backend/src/server/api/endpoints/notes/children.ts
@@ -89,11 +89,11 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			let notes = await query.limit(ps.limit).getMany();
 
 			notes = notes.filter(note => {
-				if (note.user?.isSilenced && me && followings && note.userId !== me.id && !followings[note.userId]) return false;
-				if (!me && note.user?.isSilenced) return false;
+				//if (note.user?.isSilenced && me && followings && note.userId !== me.id && !followings[note.userId]) return false;
+				//if (!me && note.user?.isSilenced) return false;
 				if (note.user?.isSuspended) return false;
-				if (this.utilityService.isBlockedHost(metaSvc.blockedHosts, note.userHost)) return false;
-				if (this.utilityService.isSilencedHost(metaSvc.silencedHosts, note.userHost)) return false;
+				//if (this.utilityService.isBlockedHost(metaSvc.blockedHosts, note.userHost)) return false;
+				//if (this.utilityService.isSilencedHost(metaSvc.silencedHosts, note.userHost)) return false;
 				return true;
 			});
 
