bc94778135ba2155269d56f6de7ae6149bb8d224
丈槍由紀 <chikorita157@ateliershiori.moe>
2024-09-14 17:05:50 -0400

    Revert "fix tab order"
    This reverts commit 527d5af1eab04d8e58bd76dd5e2d38faddc87bb9.

--- packages/frontend/src/components/MkTimeline.vue
+++ packages/frontend/src/components/MkTimeline.vue
@@ -29,7 +29,7 @@ import { defaultStore } from '@/store.js';
 import { Paging } from '@/components/MkPagination.vue';
 
 const props = withDefaults(defineProps<{
-	src: 'home' | 'local' | 'social' | 'media'  | 'bubble' | 'bubblemedia' | 'global' | 'mentions' | 'directs' | 'list' | 'antenna' | 'channel' | 'role';
+	src: 'home' | 'local' | 'media' | 'social' | 'bubble' | 'bubblemedia' | 'global' | 'mentions' | 'directs' | 'list' | 'antenna' | 'channel' | 'role';
 	list?: string;
 	antenna?: string;
 	channel?: string;
