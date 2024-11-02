5d1a422906d42dcc7788794c9b3ffca99e9bb9df
丈槍由紀 <chikorita157@ateliershiori.moe>
2024-11-01 21:40:58 -0400

    attempt fix

--- packages/backend/src/server/api/endpoints/notes/children.ts
+++ packages/backend/src/server/api/endpoints/notes/children.ts
@@ -86,7 +86,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			}
 
 			const [
-				followings,
+				followings2,
 			] = me ? await Promise.all([
 				this.cacheService.userFollowingsCache.fetch(me.id),
 			]) : [undefined];
@@ -95,7 +95,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			const notes = await query.limit(ps.limit).getMany();
 
 			notes = notes.filter(note => {
-				if (note.user?.isSilenced && me && followings && note.userId !== me.id && !followings[note.userId]) return false;
+				if (note.user?.isSilenced && me && followings2 && note.userId !== me.id && !followings2[note.userId]) return false;
 				if (!me && note.user?.isSilenced) return false;
 				if (note.user?.isSuspended) return false;
 				if (this.utilityService.isBlockedHost(metasvc.blockedHosts, note.userHost)) return false;
